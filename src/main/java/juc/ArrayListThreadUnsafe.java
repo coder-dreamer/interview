82b6d8eb4a2130978691c9ab07c82ee44e50c5fc
李海鹏 <531372791@qq.com>
2021-03-28 21:41:09 +0800

    juc面试-Collection线程不安全的举例

--- src/main/java/juc/ArrayListThreadUnsafe.java
+++ src/main/java/juc/ArrayListThreadUnsafe.java
@@ -2,12 +2,8 @@ package juc;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
+import java.util.concurrent.*;
 
 /**
  * @author 53137
@@ -16,7 +12,80 @@ import java.util.concurrent.TimeUnit;
 public class ArrayListThreadUnsafe {
     public static void main(String[] args) throws InterruptedException {
         //test();
-        test1();
+        //test1();
+        //test2();
+        //test3();
+        //test4();
+        //test5();
+        test6();
+
+    }
+
+    /**
+     * ConcurrentHashMap解决ConcurrentModificationException
+     */
+    private static void test6() {
+        Map<String, String> map = new ConcurrentHashMap<>();
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                map.put(Thread.currentThread().getName(), UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(map.toString());
+            }, "t" + i).start();
+        }
+    }
+
+    /**
+     * CopyOnWriteArraySet解决ConcurrentModificationException
+     * 底层CopyOnWriteArrayList
+     */
+    private static void test5() {
+        Set<String> set = new CopyOnWriteArraySet<>();
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                set.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(set.toString());
+            }, "t" + i).start();
+        }
+    }
+
+
+    /**
+     * 使用CopyOnWriteArrayList解决ConcurrentModificationException
+     */
+    private static void test4() {
+        List<String> list = new CopyOnWriteArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list.toString());
+            }, "t" + i).start();
+        }
+    }
+
+    /**
+     * Collections.synchronizedList解决ConcurrentModificationException
+     */
+    private static void test3() {
+        List<String> list = Collections.synchronizedList(new ArrayList<>());
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list.toString());
+            }, "t" + i).start();
+        }
+    }
+
+    /**
+     * Vector解决ConcurrentModificationException
+     */
+    private static void test2() {
+        List<String> list = new Vector<>();
+        for (int i = 0; i < 10; i++) {
+            new Thread(() -> {
+                list.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(list.toString());
+            }, "t" + i).start();
+        }
     }
 
     /**
